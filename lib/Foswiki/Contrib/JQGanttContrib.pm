d95d8d82ce0178fe7212dd4518a905c3158a105b
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2014-03-15 09:31:38 +0000

    Item12784: fixed syntax error in contrib stub
    git-svn-id: http://svn.foswiki.org/trunk/JQGanttContrib@17350 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/JQGanttContrib.pm
+++ lib/Foswiki/Contrib/JQGanttContrib.pm
@@ -3,13 +3,13 @@ package Foswiki::Contrib::JQGanttContrib;
 use strict;
 use warnings;
 
-our $VERSION = '$Rev$';
+our $VERSION = '52f46db';
 our $RELEASE = '52f46db';
 our $SHORTDESCRIPTION = 'A simple gantt chart widget';
 our $NO_PREFS_IN_TOPIC = 1;
 
 sub init {
-  require Foswiki::Plugins::JQueryPlugin ();
+  require Foswiki::Plugins::JQueryPlugin;
   Foswiki::Plugins::JQueryPlugin::registerPlugin("gantt", "Foswiki::Contrib::JQGanttContrib::Core");
 }
 
